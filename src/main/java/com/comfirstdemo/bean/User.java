8b654f4745956e2f90ec17d74ae38bd425f063dc
陈 <2276404314@qq.com>
2021-08-13 00:37:13 +0800

    第二次更新:实体类中引入lombok,减少代码量。

--- src/main/java/com/comfirstdemo/bean/User.java
+++ src/main/java/com/comfirstdemo/bean/User.java
@@ -1,5 +1,13 @@
 package com.comfirstdemo.bean;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data   //生成get,set,toString等方法
+@AllArgsConstructor   //生成有参构造函数
+@NoArgsConstructor    //无参构造
 public class User {
 
     private Integer id;
@@ -8,66 +16,57 @@ public class User {
     private String email;
     private String address;
 
-    public User(Integer id, String name, String password, String email, String address) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-        this.address = address;
-    }
+//    public User() {
+//    }
 
-    public User() {
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
 
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", password='" + password + '\'' +
+//                ", email='" + email + '\'' +
+//                ", address='" + address + '\'' +
+//                '}';
+//    }
 }
--- src/main/java/com/comfirstdemo/controller/UserController.java
+++ src/main/java/com/comfirstdemo/controller/UserController.java
@@ -4,17 +4,14 @@ import com.comfirstdemo.bean.User;
 import com.comfirstdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
  * @author hp
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 
 public class UserController {
