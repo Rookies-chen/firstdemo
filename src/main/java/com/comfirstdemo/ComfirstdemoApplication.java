b18320ada34c859ea3bfb9aa059f69fec403bdd1
陈 <2276404314@qq.com>
2021-08-13 14:55:31 +0800

    第三次更新:集成mybatis   1.pom.xml文件中引入mybatis依赖；   2.springboot的配置文件中配置mybatis；   3.dao层中的mapper文件中去掉用注解写的sql语句，改变到mapper.xml文件中。

--- src/main/java/com/comfirstdemo/ComfirstdemoApplication.java
+++ src/main/java/com/comfirstdemo/ComfirstdemoApplication.java
@@ -1,5 +1,6 @@
 package com.comfirstdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * @author hp
  */
+@MapperScan("com.comfirstdemo.dao")  //扫描dao层文件路径
 @SpringBootApplication(scanBasePackages = "com")
 //@ComponentScan(basePackages = {"com.comfirstdemo.dao"})
 public class ComfirstdemoApplication {
--- src/main/java/com/comfirstdemo/dao/UserDao.java
+++ src/main/java/com/comfirstdemo/dao/UserDao.java
@@ -2,26 +2,50 @@ package com.comfirstdemo.dao;
 
 import com.comfirstdemo.bean.User;
 import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * @author hp
  */
-@Mapper
+//@Mapper   //将该接口实体注入容器管理
+@Repository   //将该接口实体注入容器管理
 public interface UserDao {
-    @Select("select * from user where id=#{id}")
+
+    //@Select("select * from user where id=#{id}")
+    /**
+     * 根据id查询
+     * @param id
+     * @return user
+     */
     User queryUserById(Integer id);
 
-    @Select("select * from user")
+    //@Select("select * from user")
+    /**
+     * 查询所有用户
+     * @return List<User>
+     */
     List<User> queryAllUser();
 
-    @Insert("insert into user (name,password,email,address) values(#{name},#{password},#{email},#{address})")
+    //@Insert("insert into user (name,password,email,address) values(#{name},#{password},#{email},#{address})")
+    /**
+     * 添加用户
+     * @param user
+     */
     void addUser(User user);
 
-    @Delete("delete from user where id = #{id}")
+    //@Delete("delete from user where id = #{id}")
+    /**
+     * 根据用户id删除
+     * @param id
+     */
     void deleteUserById(Integer id);
 
-    @Update("update user set name=#{name},password=#{password},email=#{email},address=#{address} where id = #{id}")
+    //@Update("update user set name=#{name},password=#{password},email=#{email},address=#{address} where id = #{id}")
+    /**
+     * 更新用户信息
+     * @param user
+     */
     void updateUser(User user);
 }
